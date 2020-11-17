f610b4fce0d17a28da20d7280033f47c8d19cca7
Igor <igorko.by@gmail.com>
2020-11-17 15:22:22 +0300

    init: start project with template

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// import classNames from 'classnames'
+import './App.scss'
+
+const App = () => {
+
+  return (
+    <form className="app" onSubmit={(e) => e.preventDefault()}>
+    </form>
+  )
+}
+
+export default App
